a12cc37bee7e7ad0b1260ce7374ff4a340d28eb2
Kamal Isayev <isayev.kamal@gmail.com>
2026-06-10 14:50:07 +0200

    chore: initial project setup

---
+++ src/main/java/de/kamal/neurocards/NeurocardsApplication.java
@@ -0,0 +1,13 @@
+package de.kamal.neurocards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeurocardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeurocardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/kamal/neurocards/NeurocardsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kamal.neurocards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeurocardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
